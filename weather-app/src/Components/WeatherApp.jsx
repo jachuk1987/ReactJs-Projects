02519095c85c5c021fc2b9ccf98bf4db234530a8
jachuk1987 <jachu.k1212@gmail.com>
2024-08-13 22:38:15 +0530

    ADD

--- weather-app/src/Components/WeatherApp.jsx
+++ weather-app/src/Components/WeatherApp.jsx
@@ -33,7 +33,7 @@ const WeatherApp = () => {
       return;
     }
     try {
-      const url = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}';
+      const url = "https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}";
 
       const response = await fetch(url);
       const data = await response.json();
