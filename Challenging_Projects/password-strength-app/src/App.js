ce1caeeccc46961f9008da36367dff39ceace825
jachuk1987 <jachu.k1212@gmail.com>
2024-08-15 09:08:51 +0530

    ADD

---
+++ Challenging_Projects/password-strength-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PasswordStrength from './Components/PasswordStrength';
+
+function App() {
+  return (
+    <div className="App">
+      <PasswordStrength/>
+    </div>
+  );
+}
+
+export default App;
