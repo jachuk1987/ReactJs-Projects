23dd2284fc958b9ad5ae03897317463343276342
jachuk1987 <jachu.k1212@gmail.com>
2024-08-17 09:18:45 +0530

    ADD

---
+++ Challenging_Projects/crypto-currency-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CryptoCurrency from './Components/CryptoCurrency';
+
+function App() {
+  return (
+    <div className="App">
+      <CryptoCurrency/>
+    </div>
+  );
+}
+
+export default App;
