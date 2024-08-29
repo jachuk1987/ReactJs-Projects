af2575f891c6b2135ee8b9c363bcbe6392420cf5
jachuk1987 <jachu.k1212@gmail.com>
2024-08-29 13:27:40 +0530

    ADD

---
+++ New_Projects/app02-qrcode-withimage/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import QRCodeWithImage from './Components/QrCodeWithImage';
+
+
+function App() {
+  return (
+    <div className="App">
+      <QRCodeWithImage/>
+    </div>
+  );
+}
+
+export default App;
